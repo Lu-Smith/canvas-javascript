1a531ebd5058d1ceef8fd0f57f49ccc2384861c5
Lu-Smith <artistlunasmith@gmail.com>
2022-06-15 09:55:43 +0100

    Updated colors

--- script.js
+++ script.js
@@ -40,8 +40,22 @@ class Particle {
     if (this.size > 0.2) this.size -= 0.1;
   }
   draw() {
-    ctx.fillStyle = `#F4646A`;
-    ctx.strokeStyle = `#FFBC6C`;
+    let dx = mouse.x - this.x;
+    let dy = mouse.y - this.y;
+    let distance = Math.sqrt(dx * dx + dy * dy);
+    if (distance < 150) {
+      ctx.fillStyle = `#FFE670`;
+      ctx.strokeStyle = `white`;
+    } else if (distance > 300 && distance < 450) {
+      ctx.fillStyle = `#5DF8DB`;
+      ctx.strokeStyle = `#415EBC`;
+    } else if (distance > 450) {
+      ctx.fillStyle = `#D7F2C1`;
+      ctx.strokeStyle = `#A3D09B`;
+    } else {
+      ctx.fillStyle = `#F4646A`;
+      ctx.strokeStyle = `#F9DECE`;
+    }
     ctx.lineWidth = 5;
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
